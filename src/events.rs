197d5344fefaedc2cbf6f5f6c8a497293d0863c2
Cobord <ammar.s.husain@gmail.com>
2023-10-21 13:57:37 -0400

    shared mutable resource as a test without the printing

--- src/events.rs
+++ src/events.rs
@@ -268,68 +268,69 @@ where
 }
 
 mod test {
+    use super::Interleaves;
+
+    #[derive(PartialEq, Eq, Hash, Copy, Clone)]
+    #[repr(transparent)]
+    struct ABTemp(bool);
+    impl<T> Interleaves<T> for ABTemp {
+        fn do_interleave(&self, _: &T, other: &Self, _: &T) -> bool {
+            self == other
+        }
+
+        fn interleaves_with_everything(&self, _my_aux: &T) -> bool {
+            false
+        }
+    }
+    use std::fmt::{Debug, Error, Formatter};
+    impl Debug for ABTemp {
+        fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+            write!(f, "{:?}", self.0)
+        }
+    }
 
     #[test]
     fn two_events() {
         use std::{sync::mpsc, thread, time::Duration};
 
-        use crate::events::{Emitter, Interleaves};
-
-        #[derive(PartialEq, Eq, Hash, Copy, Clone)]
-        #[repr(transparent)]
-        struct AB(bool);
-        impl Interleaves<u64> for AB {
-            fn do_interleave(&self, _: &u64, other: &Self, _: &u64) -> bool {
-                self == other
-            }
-
-            fn interleaves_with_everything(&self, _my_aux: &u64) -> bool {
-                false
-            }
-        }
-        use std::fmt::{Debug, Error, Formatter};
-        impl Debug for AB {
-            fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-                write!(f, "{:?}", self.0)
-            }
-        }
+        use super::Emitter;
 
         let (tx, rx) = mpsc::channel();
-        let mut emitter: Emitter<AB, u64, ()> = Emitter::new(Some(tx));
-        let true_new = emitter.on(AB(true), |wait_time| {
+        let mut emitter: Emitter<ABTemp, u64, ()> = Emitter::new(Some(tx));
+        let true_new = emitter.on(ABTemp(true), |wait_time| {
             thread::sleep(Duration::from_secs(wait_time));
         });
         assert!(true_new);
-        let false_new = emitter.on(AB(false), |wait_time| {
+        let false_new = emitter.on(ABTemp(false), |wait_time| {
             thread::sleep(Duration::from_secs(wait_time));
         });
         assert!(false_new);
         let mut total_emissions = 0;
-        let mut expected_emissions: Vec<(AB, u64, ())> = Vec::with_capacity(3);
+        let mut expected_emissions: Vec<(ABTemp, u64, ())> = Vec::with_capacity(3);
 
-        let emission_now = (AB(true), 2);
+        let emission_now = (ABTemp(true), 2);
         expected_emissions.push((emission_now.0, emission_now.1, ()));
 
         let (exists, spawn_later, spawned) = emitter.emit(emission_now.0, emission_now.1);
         total_emissions += 1;
         assert!(exists && spawned && !spawn_later);
 
-        let emission_now = (AB(true), 1);
+        let emission_now = (ABTemp(true), 1);
         expected_emissions.push((emission_now.0, emission_now.1, ()));
         let (exists, spawn_later, spawned) = emitter.emit(emission_now.0, emission_now.1);
         total_emissions += 1;
         assert!(exists && spawned && !spawn_later);
 
-        let emission_now: (AB, u64) = (AB(false), 1);
+        let emission_now: (ABTemp, u64) = (ABTemp(false), 1);
         expected_emissions.push((emission_now.0, emission_now.1, ()));
         let (exists, spawn_later, spawned) = emitter.emit(emission_now.0, emission_now.1);
         total_emissions += 1;
         assert!(exists && !spawned && spawn_later);
 
-        let true_used_to_exist = emitter.off(AB(true));
+        let true_used_to_exist = emitter.off(ABTemp(true));
         assert!(true_used_to_exist);
 
-        let (exists, spawn_later, spawned) = emitter.emit(AB(true), 10);
+        let (exists, spawn_later, spawned) = emitter.emit(ABTemp(true), 10);
         assert!(!exists && !spawned && !spawn_later);
 
         emitter.wait_for_all(Duration::from_millis(250));
@@ -346,5 +347,78 @@ mod test {
         let final_wait = Duration::from_millis(250);
         let next_item = rx.recv_timeout(final_wait);
         assert!(next_item.is_err());
+    }
+
+    #[test]
+    fn common_resource() {
+        use std::{
+            sync::{Arc, Mutex},
+            cmp::max,
+            thread,
+            time::{Duration, Instant},
+        };
+        use super::Emitter;
+    
+        let a = |(other_operand, wait_millis, data1): (i32, u64, Arc<Mutex<i32>>)| {
+            thread::sleep(Duration::from_millis(wait_millis));
+            let mut data = data1.lock().unwrap();
+            *data += other_operand;
+        };
+    
+        let b = |(other_operand, wait_millis, data2): (i32, u64, Arc<Mutex<i32>>)| {
+            thread::sleep(Duration::from_millis(wait_millis));
+            let mut data = data2.lock().unwrap();
+            *data *= other_operand;
+        };
+    
+        let mut emitter: Emitter<ABTemp, (i32, u64, Arc<Mutex<i32>>), ()> = Emitter::new(None);
+        let true_new = emitter.on(ABTemp(true), a);
+        assert!(true_new);
+        let false_new = emitter.on(ABTemp(false), b);
+        assert!(false_new);
+    
+        let mut exp_data_val = 1;
+        let data = Arc::new(Mutex::new(exp_data_val));
+    
+        let mut other_operand;
+        let mut expected_time = Duration::from_millis(0);
+        let mut serial_time = Duration::from_millis(0);
+    
+        other_operand = 1;
+        let wait_time_1 = 50;
+        emitter.emit(ABTemp(true), (other_operand, wait_time_1, data.clone()));
+        exp_data_val += other_operand;
+    
+        other_operand = 10;
+        let wait_time_2 = 60;
+        emitter.emit(ABTemp(true), (other_operand, wait_time_2, data.clone()));
+        exp_data_val += other_operand;
+    
+        expected_time += Duration::from_millis(max(wait_time_1, wait_time_2));
+        serial_time += Duration::from_millis(wait_time_1);
+        serial_time += Duration::from_millis(wait_time_2);
+    
+        other_operand = 2;
+        let wait_time_3 = 50;
+        emitter.emit(ABTemp(false), (other_operand, wait_time_3, data.clone()));
+        exp_data_val *= other_operand;
+        expected_time += Duration::from_millis(wait_time_3);
+        serial_time += Duration::from_millis(wait_time_3);
+    
+        other_operand = 24;
+        let wait_time_4 = 20;
+        emitter.emit(ABTemp(true), (other_operand, wait_time_4, data.clone()));
+        exp_data_val += other_operand;
+        expected_time += Duration::from_millis(wait_time_4);
+        serial_time += Duration::from_millis(wait_time_4);
+    
+        let loop_time = Duration::from_millis(15);
+        let now = Instant::now();
+        emitter.wait_for_all(loop_time);
+        let elapsed = now.elapsed();
+        assert!(elapsed < expected_time + 3 * loop_time);
+        assert!(elapsed < serial_time);
+        let data_val = *data.lock().unwrap();
+        assert_eq!(data_val, exp_data_val);
     }
 }
